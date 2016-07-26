db3321b41dfbff79be3c713feb469db4177da52b
David Bradway <dpb6@duke.edu>
2016-07-26 09:52:51 -0400

    refactor with buttons, callbacks, globals, and subdirs

--- tkintergui.py
+++ tkintergui.py
@@ -6,14 +6,42 @@
 import sys, getopt, os, tkinter
 from subprocess import call
 
-def callback(name):
-    # Maybe make this a switch case with all your imaging cases and scripts?
-    print('/getlab/dpb6/repos/pygui/4V1c/'+name)
-    os.chdir('/getlab/dpb6/repos/pygui/4V1c/'+name)
-    print(os.getcwd())
-    if 'Active' in name:
+baseDir = '/getlab/dpb6/repos/pygui/4V1c/'
+activePassive = 'Active'
+focus = 25
+
+def activepassive_callback():
+    global baseDir
+    global activePassive
+    global focus
+
+    if 'Active' in activePassive:
+        activePassive = 'Passive'
+        os.chdir(os.path.join(baseDir,activePassive,focus))
+        print(os.getcwd())
+    elif 'Passive' in activePassive:
+        activePassive = 'Active'
+        os.chdir(os.path.join(baseDir,activePassive,focus))
+        print(os.getcwd())
+
+    
+def foc_callback(btn_value):
+    global baseDir
+    global activePassive
+    global focus
+
+    if 'Foc' in btn_value:
+        focus = btn_value
+        os.chdir(os.path.join(baseDir,activePassive,focus))
+        print(os.getcwd())
+    
+
+def go_callback():
+    global activePassive
+
+    if 'Active' in activePassive:
         call(["python", "grab_TTE_Mixed.py"])
-    elif 'Passive' in name:
+    elif 'Passive' in activePassive:
         call(["python", "grab_TTE_all.py"])
 
 def main(argv):
@@ -25,7 +53,7 @@ def main(argv):
         command_list = argv
     else:
         # Else, use the defaults
-        command_list = ['Active/Foc_20mm','Active/Foc_25mm','Passive/Foc_20mm','Passive/Foc_25mm']
+        command_list = ['Foc_20mm','Foc_25mm','Foc_30mm','Foc_35mm','Foc_40mm']
     
     top=tkinter.Tk()
     
@@ -33,12 +61,18 @@ def main(argv):
     btn=[]
     # Add a button for each item in the command list
     for i in range(len(command_list)):
-        btn.append(tkinter.Button(top,text=str(command_list[i]), command=lambda command_name=command_list[i]:callback(command_name)))
+        btn.append(tkinter.Button(top,text=str(command_list[i]), command=lambda btn_value=command_list[i]:foc_callback(btn_value)))
         # Put each button in a new row, in one column
-        btn[-1].grid(row=i, column=0)
+        btn[-1].grid(row=0, column=i)
+
+    btn.append(tkinter.Button(top,text='Active/Passive', command=lambda:activepassive_callback()))
+    btn[-1].grid(row=1, column=0)
+
+    btn.append(tkinter.Button(top,text='Go!', command=lambda:go_callback()))
+    btn[-1].grid(row=1, column=1)
 
     top.mainloop()
 
 if __name__ == "__main__":
-    # Strip off automatic first argument (sys.argv[0] is the program ie. script name)
+    # Strip off automatic first argument (sys.argv[0] is the program ie. script btn_value)
     main(sys.argv[1:])
